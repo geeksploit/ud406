1396eee168745f652c9a84fa0657d66502fddd4a
geeksploit <geeksploit@shootan.com>
2017-09-13 21:17:34 +0300

    Create GameplayScreen class.

---
+++ 2.2.02-Exercise-TheProjectSkeleton/core/src/com/udacity/gamedev/gigagal/GameplayScreen.java
@@ -0,0 +1,10 @@
+package com.udacity.gamedev.gigagal;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+/**
+ * Created by geeksploit on 13.09.2017.
+ */
+
+public class GameplayScreen extends ScreenAdapter {
+}
