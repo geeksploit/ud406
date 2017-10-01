873e6fdb31e57ffe96bb7e2814abea65baabf448
geeksploit <geeksploit@shootan.com>
2017-10-01 21:57:00 +0300

    Define a constant for world size

--- 2.2.03-Exercise-AssetLoading/core/src/com/udacity/gamedev/gigagal/util/Constants.java
+++ 2.2.03-Exercise-AssetLoading/core/src/com/udacity/gamedev/gigagal/util/Constants.java
@@ -6,14 +6,7 @@ public class Constants {
 
     public static Color BACKGROUND_COLOR = Color.SKY;
 
-    // TODO: Set a WORLD_SIZE
-    /**
-     * We'll draw our sprites at their natural size, so this is the number of pixels of our Pixel
-     * art that will fit on the screen. We're going to use this size to initialize both dimensions
-     * of an ExtendViewport, and we'll run the game in landscape mode, so this will really end up
-     * specifying the height of the world. We recommend 128.
-     */
-
+    public static float WORLD_SIZE = 128;
 
     public static final String TEXTURE_ATLAS = "images/gigagal.pack.atlas";
 
