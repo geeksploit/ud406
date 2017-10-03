418b73e791b823217b2c32142597e3b762f1159f
geeksploit <geeksploit@shootan.com>
2017-10-03 16:02:48 +0300

    Define a constant for GigaGal's eye position

--- 2.2.04-Exercise-DrawGigaGal/core/src/com/udacity/gamedev/gigagal/util/Constants.java
+++ 2.2.04-Exercise-DrawGigaGal/core/src/com/udacity/gamedev/gigagal/util/Constants.java
@@ -1,6 +1,7 @@
 package com.udacity.gamedev.gigagal.util;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Vector2;
 
 public class Constants {
     public static final Color BACKGROUND_COLOR = Color.SKY;
@@ -9,8 +10,7 @@ public class Constants {
     public static final String TEXTURE_ATLAS = "images/gigagal.pack.atlas";
     public static final String STANDING_RIGHT = "standing-right";
 
-    // TODO: Define a Vector2 Constant for GigaGal's eye position within her sprites (16, 24)
-
+    public static final Vector2 GIGAGAL_EYE_POSITION = new Vector2(16, 24);
 
     public static final float GIGAGAL_EYE_HEIGHT = 16;
 
