630bc4d990a02bdfe1644bf2c55c3abca1a9641d
geeksploit <geeksploit@shootan.com>
2017-10-03 15:53:11 +0300

    Declare position vector

--- 2.2.04-Exercise-DrawGigaGal/core/src/com/udacity/gamedev/gigagal/entities/GigaGal.java
+++ 2.2.04-Exercise-DrawGigaGal/core/src/com/udacity/gamedev/gigagal/entities/GigaGal.java
@@ -1,13 +1,13 @@
 package com.udacity.gamedev.gigagal.entities;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class GigaGal {
 
     public final static String TAG = GigaGal.class.getName();
 
-    // TODO: Add a position
-
+    private Vector2 position;
 
     public GigaGal() {
         // TODO: Initialize GigaGal's position with height = GIGAGAL_EYE_HEIGHT
